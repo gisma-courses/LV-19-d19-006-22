022b2866c79aaf457bf31f5cd5aca00349309464
gisma <gisma@users.noreply.github.com>
2022-11-01 08:52:11 +0000

    published by GitHub Actions

---
+++ schedule/schedule-info.js
@@ -0,0 +1,14 @@
+moreaEventData = [
+  {title: "Assessment R-spatial GIS", url: "/LV-19-d19-006-22/morea/geocomputation-with-r/assessment-spatial-r-gis-1.html", start: "2021-11-09T23:59"},
+  {title: "Apply different models for microclimate analysis and write a shorte note", url: "/LV-19-d19-006-22/morea/microclimate-analysis/assessment-microclimate-analysis-gis-1.html", start: "2022-02-15T09:00"},
+  {title: "Use the course scripts repository", url: "/LV-19-d19-006-22/morea/microclimate-analysis/experience-microclimate-analysis-gis-1.html", start: "2022-01-28T23:00"},
+  {title: "Reproducible Research", url: "/LV-19-d19-006-22/modules/gis-reproducible-research", start: "2022-11-01", end: "2022-11-08"},
+  {title: "Assessment Website for Course Documentation", url: "/LV-19-d19-006-22/morea/gis-reproducible-research/assessment-gis-reproducible-research.html", start: "2021-11-16T23:59"},
+  {title: "Write a short communication about your project", url: "/LV-19-d19-006-22/morea/scientific-reporting/experience-scientific-writing-1.html", start: "2022-02-14T23:00"},
+  {title: "Write a short note", url: "/LV-19-d19-006-22/morea/scientific-reporting/assessment-scientific-writing-1.html", start: "2022-02-15T09:00"},
+  {title: "Assessment getting organized", url: "/LV-19-d19-006-22/morea/prerequisites/assessment-pre-know-how-1.html", start: "2021-11-02T23:59"},
+  {title: "Knowledge and prerequisites for the course", url: "/LV-19-d19-006-22/modules/module-prerequisites", start: "2022-11-01", end: "2022-11-08"},
+  {title: "Assessment: LiDAR based Forest Indices", url: "/LV-19-d19-006-22/morea/forest-lidar/assessment-forest-lidar-gis-1.html", start: "2021-11-30T23:59"},
+  {title: "Methodological Warm Up", url: "/LV-19-d19-006-22/modules/drain", start: "2022-02-15T23:00"},
+  {title: "Write a short note", url: "/LV-19-d19-006-22/morea/drain/assess-1.html", start: "2022-02-15T09:00"}
+]
